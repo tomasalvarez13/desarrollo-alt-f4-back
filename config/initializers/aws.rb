ee8e94b6806380a53392d3ad17238d0ee019146f
David Weinstein <dweinstein@uc.cl>
2022-12-11 22:17:19 -0300

    feat(aws): add aws config

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,11 @@
 require 'aws-sdk-s3'
 
+# obviously this file wont work in github actions because it needs the env variables to be set
+
 Aws.config.update({
-  region: ENV['AWS_REGION'],
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+  region: 'us-east-1',
+  credentials: Aws::Credentials.new('AKIAXBOXQNGXMMYSZ42M',
+    '0Sgfupj9iFCPe5ZxBESgmB2Wpb0rUjCKbNB0QISv'),
 })
 
 S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
